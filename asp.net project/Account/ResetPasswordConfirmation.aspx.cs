08c51f98947154e56e82da46426a1d1cafc29041
Scott Guyton <kiljoy001@gmail.com>
2017-01-17 15:32:06 -0800

    Add project files.

---
+++ asp.net project/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace asp.net_project.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ asp.net project/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_project.Startup))]
+namespace asp.net_project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
